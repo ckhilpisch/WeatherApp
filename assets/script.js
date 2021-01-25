0f9c4f6b2652e8c9105940ecc942e246dea64821
CaroKris <ckhilpisch@gmail.com>
2021-01-25 11:18:19 -0600

    adding nested call for latitude and longitude

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,37 @@
 //API call for one city, 5 days forecast api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // API key for the uv index (requires lat and long)
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-var apiKey = "73d3cee72322c512646546f162d5afe5"
-var cityName = "Minneapolis"
-var lat = "";
-var long = "";
-
+var apiKey = "73d3cee72322c512646546f162d5afe5";
+var cityName = "minneapolis";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+// var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 
-var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?"+ lat + "&" + lon + "&exclude=minutely, hourly, daily&appid=" + apiKey;
+// console.log(queryURL);
+// console.log(queryURL2);
+// console.log(queryURL3);
 
-var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
 
+    // code for intial information response
+    console.log(response);
 
-console.log(queryURL);
-console.log(queryURL2);
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+    
+        // code for the UV index response
+    
+        console.log(response);
+    
+    });
+
+});
